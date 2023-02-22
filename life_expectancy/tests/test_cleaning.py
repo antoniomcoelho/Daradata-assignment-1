46ff8ecc927aca811d9a0c13b8d921f5951ead17
antoniomcoelho <84011886+antoniomcoelho@users.noreply.github.com>
2023-02-22 15:31:21 +0000

    Update test_cleaning.py

--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -7,7 +7,9 @@ from . import OUTPUT_DIR
 
 def test_clean_data(pt_life_expectancy_expected):
     """Run the `clean_data` function and compare the output to the expected output"""
-    main()
+    CSV_TABLE = load_data()
+    DF_FINAL = clean_data(CSV_TABLE, 'PT')
+    save_data(DF_FINAL)
     pt_life_expectancy_actual = pd.read_csv(
         OUTPUT_DIR / "pt_life_expectancy.csv"
     )
