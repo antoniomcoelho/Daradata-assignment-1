39f6d557975744adaefcd65e063ec231fbc84175
amcoelho <antonio.m.coelho@inesctec.pt>
2023-02-22 14:37:19 +0000

    clean clean_data - before assigment 2

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -6,70 +6,83 @@ import pandas as pd
 
 DATA_DIR = Path(__file__).parent / 'data'
 
+
+
+
+
 def clean_data(region_user):
     ''' Function to clean data from eu_life_expectancy_raw.tsv file'''
-    # Read tsv file and transform it to csv
-    csv_table = read_file()
+    csv_table = read_data()
 
-    column0 = csv_table.columns[0] # Name of first column
-    data = [] # Vector with all fields
-    for i in range(0, 10): # len(csv_table)
+    first_column = csv_table.columns[0]
+    df_life = []
+
+    for i in range(0, 10):  # len(csv_table)
         for j in range(1, len(csv_table.columns)):
             # Split the first column into unit, sex, age and region
-            data_prov = csv_table[column0][i].split(',')
+            df_first_column = csv_table[first_column][i].split(',')
 
             # Convert value to float
             value, flag_value_is_not_float = convert_value_to_float(csv_table, j, i)
 
             # Append necessary information (unit, sex, age and region, year)
-            data = append_information(data_prov, data, flag_value_is_not_float,
-                                      region_user, csv_table, value, j)
-
+            df_life = append_information(df_first_column, df_life, flag_value_is_not_float,
+                                         region_user, csv_table, value, j)
 
     # Create dataframe and rename columns
-    columns_name = {0:'unit', 1:'sex', 2:'age', 3:'region', 4:'year', 5:'value'}
-    df_data = pd.DataFrame(data).rename(columns=columns_name)
+    columns_name = {0: 'unit', 1: 'sex', 2: 'age', 3: 'region', 4: 'year', 5: 'value'}
+    df_life = pd.DataFrame(df_life).rename(columns=columns_name)
 
-    # Save csv
-    df_data.to_csv('pt_life_expectancy.csv', index=False)
+    # Save data
+    df_life.to_csv('pt_life_expectancy.csv', index=False)
 
-def read_file():
+    return df_life
+
+def read_data():
     ''' Read tsv file and transform it to csv '''
     name_file = DATA_DIR / "eu_life_expectancy_raw.tsv"
     csv_table = pd.read_table(name_file, sep='\t')
 
     return csv_table
 
+
 def convert_value_to_float(csv_table, j, i):
     ''' Convert value to float '''
     value = 0
-    flag = 0  # Indicate if the value is not a value convertible to float (ex. ":") 
 
-    # If the value starts with a number (ex. "42.1")
+    # 0 - it is convertible, 1 - it is not convertible
+    flag_value_is_not_convertible = 0
+
+    # If the value starts with a number (ex. "42.1"),
+    # otherwise it is a word and not convertible to float
     if str(csv_table[csv_table.columns[j]][i])[0].isdigit():
         # Split the value when it has a number and a letter (ex. "42.1 e")
         value = str(csv_table[csv_table.columns[j]][i]).split(" ", maxsplit=1)[0]
-        # Convert value to float
         value = float(value)
     else:
-        flag = 1  # The value is not convertible to float (ex. ":")
+        flag_value_is_not_convertible = 1  # The value is not convertible to float (ex. ":")
 
-    return value, flag
+    return value, flag_value_is_not_convertible
 
-def append_information(data_prov, data, # pylint: disable=too-many-arguments
+
+def append_information(df_prov, df_life, # pylint: disable=too-many-arguments
                        flag_value_is_not_float, region_user,
                        csv_table, value, j): # pylint: disable=too-many-arguments
     ''' Append necessary information (unit, sex, age and region, year) to data vector '''
+    region_column = 3
     if flag_value_is_not_float == 0:
-        # Select regions that equal to region
-        # defined by the user (region_user)
-        if data_prov[3] == region_user:
-            # Append year to data_prov (= [unit, sex, age and region, year])
-            data_prov.append(int(csv_table.columns[j]))
-            # Append value of that year (= [unit, sex, age, region, year, value])
-            data_prov.append(value)
-            data.append(data_prov)
-    return data
+        # Filters selected regions
+        if df_prov[region_column] == region_user:
+
+            # Append "year" (= [unit, sex, age and region, year])
+            df_prov.append(int(csv_table.columns[j]))
+
+            # Append "value" of that "year" (= [unit, sex, age, region, year, value])
+            df_prov.append(value)
+            df_life.append(df_prov)
+
+    return df_life
+
 
 def add_region_user():
     ''' Function to create a command-line option to select the region'''
@@ -79,6 +92,8 @@ def add_region_user():
 
     return args.region
 
+
 if __name__ == '__main__': # pragma: no cover
     REGION_USER = add_region_user()
+
     clean_data(REGION_USER)
