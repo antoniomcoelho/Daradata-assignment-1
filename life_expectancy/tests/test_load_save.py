3142dd506a06508efb36f4200605983bfd403453
amcoelho <antonio.m.coelho@inesctec.pt>
2023-04-26 11:44:13 +0100

    review assigment 3 - test_load_save

--- life_expectancy/tests/test_load_save.py
+++ life_expectancy/tests/test_load_save.py
@@ -1,20 +1,22 @@
 """Tests for the cleaning module"""
 import pandas as pd
 import unittest.mock as mock
+from unittest.mock import patch
+from pathlib import Path # pylint: disable=import-error
 
 from life_expectancy.load_save import load_data, save_data
 from . import OUTPUT_DIR
 
+DATA_DIR = Path(__file__).parent.parent / 'data'
 
-
-def test_load_data(load_data_expected):
+@patch("life_expectancy.load_save.pd.read_table")
+def test_load_data(mock_load, load_data_expected):
     """Run the `load_data` function and compare the output to the expected output"""
 
-    load_data_actual = load_data()
+    mock_load.return_value = load_data_expected
 
-    pd.testing.assert_frame_equal(
-        load_data_actual, load_data_expected
-    )
+    name_file = DATA_DIR / "eu_life_expectancy_raw.tsv"
+    mock_load.assert_called_once_with(name_file, sep='\t')
 
 
 def test_save_data(pt_life_expectancy_expected):
