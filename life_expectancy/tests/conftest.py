5abc9eafc3097b0a285c09ae2f12795f545ca205
amcoelho <antonio.m.coelho@inesctec.pt>
2023-05-23 11:57:00 +0100

    assigment4 - cleaned files with pylint

--- life_expectancy/tests/conftest.py
+++ life_expectancy/tests/conftest.py
@@ -19,7 +19,7 @@ def load_data_expected() -> pd.DataFrame:
     return pd.read_csv(FIXTURES_DIR / "eu_life_expectancy_raw.tsv", sep="\t")
 
 @pytest.fixture(scope="session")
-def load_data_expected_JSON() -> pd.DataFrame:
+def load_data_expected_json() -> pd.DataFrame:
     """Fixture to load the expected tsv file"""
     return pd.read_json(FIXTURES_DIR / "eurostat_life_expect.json")
 
--- life_expectancy/tests/test_clean_files.py
+++ life_expectancy/tests/test_clean_files.py
@@ -1,33 +1,33 @@
 """Tests for the cleaning module"""
-import pandas as pd
-import unittest.mock as mock  # pylint: disable=consider-using-from-import
-
 from pathlib import Path # pylint: disable=import-error
+import unittest.mock as mock  # pylint: disable=consider-using-from-import
+import pandas as pd
 
 from life_expectancy.clean_files import CleanFile, Country, TSVstrategy, JSONstrategy
-from . import FIXTURES_DIR, OUTPUT_DIR
+from . import OUTPUT_DIR
 
 
 
 DATA_DIR = Path(__file__).parent.parent / 'data'
 
 
-def test_load_data_TSV(load_data_expected):
+def test_load_data_tsv(load_data_expected):
     """Run the `load_data` function and compare the output to the expected output of TSV files"""
     actual_data = TSVstrategy().load_data()
     pd.testing.assert_frame_equal(actual_data, load_data_expected)
 
 
-def test_load_data_JSON(load_data_expected_JSON):
+def test_load_data_json(load_data_expected_json):
     """Run the `load_data` function and compare the output to the expected output of JSON files"""
     actual_data = JSONstrategy().load_data()
-    pd.testing.assert_frame_equal(actual_data, load_data_expected_JSON)
+    pd.testing.assert_frame_equal(actual_data, load_data_expected_json)
 
 
 def test_clean_data_tsv(pt_life_expectancy_expected, load_data_expected):
     """Run 'clean_data` function for TSV files"""
     pt_life_expectancy_actual = TSVstrategy().clean_data(load_data_expected, ['PT'])
-    pd.testing.assert_frame_equal(pt_life_expectancy_actual[0].reset_index(drop=True), pt_life_expectancy_expected)
+    pd.testing.assert_frame_equal(
+        pt_life_expectancy_actual[0].reset_index(drop=True), pt_life_expectancy_expected)
 
 
 def test_countries_list():
@@ -44,4 +44,3 @@ def test_save_data(pt_life_expectancy_expected):
     with mock.patch.object(save_data_expected, "to_csv") as to_csv_mock:
         CleanFile(['PT'], 'TSV').save_data([save_data_expected], ['PT'])
         to_csv_mock.assert_called_with(OUTPUT_DIR / "pt_life_expectancy.csv", index=False)
-
